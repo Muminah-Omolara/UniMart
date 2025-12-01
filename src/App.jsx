391eaacc341bdfa0782bb18460fb86dacd91d013
Muminah Omolara <muminahadelabu3@gmail.com>
2025-12-02 00:50:14 +0100

    deploying on render

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Hero from "./components/Hero";
-import Nav from "./components/NAv";
+import Nav from "./components/Nav";
 import HowItWorks from "./components/HowItWorks";
 import Categories from "./components/Categories";
 import TrustSection from "./components/TrustSection";
